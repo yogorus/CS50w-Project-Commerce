c2299a94dae4482bc9cf8bdcda02d25fe3c35f6e
yogorus <yogorus@gmail.com>
2023-01-22 16:20:27 +0300

    Add Comment and Category to db, and foreign keys to Listing

--- auctions/models.py
+++ auctions/models.py
@@ -5,9 +5,31 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.URLField(blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='listings', blank=True, default='No Category')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+
+    def __str__(self):
+        return f"{self.id}: {self.title} by {self.author}"
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return f'Comment on {self.listing.id}: {self.listing.title} by {self.author}'
